87ce672056761d79568ce84d35b1d603be16cee2
Andres Marin <andres.marin@gmx.com>
2024-11-19 15:55:17 +1100

    Completed correct appearance for description field

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SelfIntroduction.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SelfIntroduction.jsx
@@ -1,7 +1,6 @@
 ﻿/* Self introduction section */
 import React, { Component } from 'react';
-import Cookies from 'js-cookie'
-
+import Cookies from 'js-cookie';
 export default class SelfIntroduction extends React.Component {
     constructor(props) {
         super(props);
@@ -13,6 +12,34 @@ export default class SelfIntroduction extends React.Component {
 
 
     render() {
+        return (
+            <div className='ui sixteen wide column'>
+                <div className='field'>
+                    <input
+                        placeholder="Please provide a short summary about yourself"
+                        maxLength="150"
+                    />
+                    <label>Summary must be no more than 150 characters.</label>
+                </div>
+                <div className='field'>
+                    <textarea
+                        placeholder="Please tell us about any hobbies, additional expertise, or anything else you’d like to add."
+                        maxLength="600"
+                        style={{ 
+                            minHeight: 100,
+                            width: '100%',
+                            resize: 'none',
+                        }}
+                    />
+                    <label>Description must be between 150-600 characters.</label>
+                </div>
+                <div className="sixteen wide column">
+                    <div>
+                        <input type="button" className="ui teal button right floated" onClick={null} value="Save"></input>
+                    </div>
+                </div>
+            </div>
+        )
        
     }
 }
