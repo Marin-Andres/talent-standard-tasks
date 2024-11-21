a0ce7fb3f28161fc2a631b7c7766acb915d71bd5
Andres Marin <andres.marin@gmx.com>
2024-11-21 21:20:35 +1100

    Validation added for Summary and Description

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SelfIntroduction.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/SelfIntroduction.jsx
@@ -1,9 +1,22 @@
 ﻿/* Self introduction section */
 import React, { Component } from 'react';
 import Cookies from 'js-cookie';
+import * as Yup from 'yup';
 export default class SelfIntroduction extends React.Component {
     constructor(props) {
         super(props);
+
+        this.schema = Yup.object().shape({
+            summary: 
+                Yup.string()
+                .required('Summary is required.')
+                .max(150,'Summary must be no more than 150 characters.'),
+            description: 
+                Yup.string()
+                .required('Description is required.')
+                .min(150,'Description must be at least 150 characters.')
+                .max(600, 'Description must be no more than 600 characters.'),
+        });
         
         this.handleChange = this.handleChange.bind(this);
         this.handleSave = this.handleSave.bind(this);
@@ -16,11 +29,14 @@ export default class SelfIntroduction extends React.Component {
     }
 
     handleSave(event) {
-        const data = {};
-        console.log("event",event);
-        data[event.target.name] = event.target.value;
-        this.props.updateProfileData(data);
-        //TODO validation
+        try {
+            const formData = {summary: this.props.summary, description: this.props.description};
+            const valid = this.schema.validateSync(formData);
+
+            this.props.updateProfileData(formData);
+        } catch(error) {
+            TalentUtil.notification.show(error, "error", null, null);
+        }
     }
     
     render() {
