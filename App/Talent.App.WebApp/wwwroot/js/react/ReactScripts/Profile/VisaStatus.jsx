8757fc5a90f7d718c93e6baa780ed1110fbe2e5f
Andres Marin <andres.marin@gmx.com>
2024-12-03 07:34:01 +1100

    Saves data back to database. pending hiding date as needed and proper date display

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/VisaStatus.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/VisaStatus.jsx
@@ -6,13 +6,21 @@ import { UTCDatePicker } from '../Form/UTCDatePicker.jsx';
 export default class VisaStatus extends React.Component {
     constructor(props) {
         super(props)
+
+        const inputDate  = this.props.visaExpiryDate ? this.props.visaExpiryDate : null;
+        console.log("inputDate", inputDate)
+        this.state = {
+            expiryDate: this.props.visaExpiryDate,
+            showDate: inputDate
+        };
         
         this.handleChange = this.handleChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
     }
 
     handleChange(event) {
         if (event.target.value) {
-            console.log("name, value",event.target.name, event.target.value)
             const newData = {};
             newData[event.target.name] = event.target.value;
             this.props.saveProfileData(newData);
@@ -20,6 +28,18 @@ export default class VisaStatus extends React.Component {
         else {
             TalentUtil.notification.show("Invalid visa type", "error", null, null);
         }
+    }
+
+    handleDateChange(date) {
+        this.setState({
+          expiryDate: date
+        });    
+    }
+
+    handleSave() {
+        const newData = {};
+        newData['visaExpiryDate'] = this.state.expiryDate;
+        this.props.saveProfileData(newData);
     }
 
     render() {
@@ -49,14 +69,14 @@ export default class VisaStatus extends React.Component {
                         <div className="field">
                             <label>Visa expiry date</label>
                             <UTCDatePicker
-                                selected={null}
-                                onChange={() => {}}
+                                selected={this.state.expiryDate}
+                                onChange={this.handleDateChange}
                             />
                         </div>
                     </div>
                     <div className='ui four wide column'>
                         <div className="field" style={{ marginTop: '1.75em'}}>
-                            <button type="button" className="ui teal button" onClick={null}>Save</button>
+                            <button type="button" className="ui teal button" onClick={this.handleSave}>Save</button>
                         </div>
                     </div>
                 </div>
