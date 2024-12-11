1ce0522fe7acb865178787fe12904a3dddd3b721
Andres Marin <andres.marin@gmx.com>
2024-12-11 14:59:56 +1100

    Load profile photo upon componentDidMount

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
@@ -7,27 +7,25 @@ export default class PhotoUpload extends Component {
     constructor(props) {
         super(props);
 
-        this.loadImages = this.loadImages.bind(this);
+        this.loadImage = this.loadImage.bind(this);
         this.selectFileToUpload = this.selectFileToUpload.bind(this);
         this.fileSelectedHandler = this.fileSelectedHandler.bind(this);
-        this.removeFile = this.removeFile.bind(this);
         this.fileUploadHandler = this.fileUploadHandler.bind(this);
         this.maxFileSize = 2097152;
         this.maxNoOfFiles = 1;
         this.acceptedFileType = ["image/gif", "image/jpeg", "image/png", "image/jpg"];
 
         this.state = {
-            selectedFile: [],
-            selectedFileName: [],
-            imageSrc: [],
-            imageId: [],
-            selectedRemoveFileId: [],
-            currentNoOfFiles: 0,
+            selectedFile: null,
+            selectedFileName: null,
+            imageSrc: null,
+            imageId: null,
+            selectedRemoveFileId: null,
             showUpload: false,
         }
     };
 
-    loadImages() {
+    loadImage() {
 
         var cookies = Cookies.get('talentAuthToken');
 
@@ -41,32 +39,15 @@ export default class PhotoUpload extends Component {
             contentType: "application/json",
             dataType: "json",
             success: function (res) {
-
-                let imageSrcArr = [];
-                let imageIdArr = [];
-                let selectedFileArr = [];
-
-                imageSrcArr.push("http://localhost:60290/profile/profile/getProfileImage/?Id=" + res.profilePath);
-                imageIdArr.push(res.profilePath);
-                selectedFileArr.push("");                
-                // if (res.employerProfile.length > 0) {
-                //     for (var i = 0; i < res.employerProfile.length; i++) {
-                //         imageSrcArr.push("http://localhost:60290/profile/profile/getProfileImage/?Id=" + res.employerProfile[i].fileName);
-                //         imageIdArr.push(res.employerProfile[i].id);
-                //         selectedFileArr.push("");
-                //     }
-                // }
-
                 this.setState({
-                    imageSrc: imageSrcArr,
-                    imageId: imageIdArr,
-                    selectedFile: selectedFileArr,
-                    selectedFileName: [],
-                    selectedRemoveFileId: [],
-                    currentNoOfFiles: res.employerProfile.length
+                    imageSrc: res.data,
                 });
             }.bind(this)
         });
+    }
+
+    componentDidMount() {
+        this.loadImage();
     }
 
     selectFileToUpload() {
@@ -81,62 +62,34 @@ export default class PhotoUpload extends Component {
         let localImageId = this.state.imageId;
         let localCurrentNoOfFiles = this.state.currentNoOfFiles;
 
-        for (let i = 0; i < event.target.files.length; i++) {
-
-            if (event.target.files[i].size > this.maxFileSize || this.acceptedFileType.indexOf(event.target.files[i].type) == -1) {
-                TalentUtil.notification.show("Max file size is 2 MB and supported file types are *.jpg, *.jpeg, *.png, *.gif", "error", null, null);
-            } else if (localCurrentNoOfFiles >= this.maxNoOfFiles) {
-                TalentUtil.notification.show("Exceed Maximum number of files allowable to upload", "error", null, null);
-            } else {
-                localSelectedFile = localSelectedFile.concat(event.target.files[i]),
-                localSelectedFileName = localSelectedFileName.concat(event.target.files[i].name),
-                localImageSrc = localImageSrc.concat(window.URL.createObjectURL(event.target.files[i])),
-                localImageId = localImageId.concat('0'),
-                localCurrentNoOfFiles = localCurrentNoOfFiles + 1
-            }
+        if (localCurrentNoOfFiles >= this.maxNoOfFiles) {
+            TalentUtil.notification.show("Exceed Maximum number of files allowable to upload", "error", null, null);
+        } 
+        else if (event.target.files[0].size > this.maxFileSize || this.acceptedFileType.indexOf(event.target.files[0].type) == -1) {
+            TalentUtil.notification.show("Max file size is 2 MB and supported file types are *.jpg, *.jpeg, *.png, *.gif", "error", null, null);
+        } 
+        else {
+            localSelectedFile = event.target.files[0];
+            localSelectedFileName = event.target.files[0].name;
+            localImageSrc = window.URL.createObjectURL(event.target.files[0]);
+            localImageId = '0';
         }
         this.setState({
             selectedFile: localSelectedFile,
             selectedFileName: localSelectedFileName,
             imageSrc: localImageSrc,
             imageId: localImageId,
-            currentNoOfFiles: localCurrentNoOfFiles,
             showUpload: true,
         })
     }
 
-    removeFile(event) {
-        let localselectedRemoveFileId = this.state.selectedRemoveFileId;
-        let localSelectedFile = this.state.selectedFile;
-        let localSelectedFileName = this.state.selectedFileName;
-        let localImageSrc = this.state.imageSrc;
-        let localImageId = this.state.imageId;
-        let localCurrentNoOfFiles = this.state.currentNoOfFiles;
-
-        localselectedRemoveFileId = localselectedRemoveFileId.concat(event.target.getAttribute('imageid'));
-        localSelectedFile.splice(event.target.getAttribute('value'), 1);
-        localSelectedFileName.splice(event.target.getAttribute('value'), 1);
-        localImageSrc.splice(event.target.getAttribute('value'), 1);
-        localImageId.splice(event.target.getAttribute('value'), 1);
-
-        this.setState({
-            selectedFile: localSelectedFile,
-            selectedFileName: localSelectedFileName,
-            imageSrc: localImageSrc,
-            imageId: localImageId,
-            selectedRemoveFileId: localselectedRemoveFileId,
-            currentNoOfFiles: this.state.currentNoOfFiles - 1
-        })
-    }
-
     fileUploadHandler() {
-        if (this.state.selectedFile.length > 0) {
+        if (this.state.selectedFile != null) {
             let data = new FormData();
-            for (var i = 0; i < this.state.selectedFile.length; i++) {
-                if (this.state.selectedFile[i] != "") {
-                    data.append('file' + i, this.state.selectedFile[i]);
-                }
+            if (this.state.selectedFile != "") {
+                data.append('file', this.state.selectedFile);
             }
+
 
             var cookies = Cookies.get('talentAuthToken');
 
@@ -154,7 +107,7 @@ export default class PhotoUpload extends Component {
                     if (res.success) {
                         TalentUtil.notification.show("Profile photo updated successfully", "success", null, null)
                         this.setState({showUpload: false});
-                        this.loadImages();
+                        this.loadImage();
                     } else {
                         TalentUtil.notification.show(res.message, "error", null, null);
                     }
@@ -169,16 +122,30 @@ export default class PhotoUpload extends Component {
     }
 
     render() {
-        let showProfileImg = [];
-
-        for (let i = 0; i < this.state.currentNoOfFiles; i++) {
-            if (this.state.imageSrc[i] != null) {
-                showProfileImg.push(<span key={i}><img style={{ height: 112, width: 112, borderRadius: 55 }} className="ui small" src={this.state.imageSrc[i]} alt="Image Not Found" /><i className="remove sign icon" onClick={this.removeFile} value={i} imageid={this.state.imageId[i]}></i></span>);
-            }
+        let showProfileImg = null;
+        if (this.state.imageSrc === null) {
+            showProfileImg = (
+                <span key="new">
+                    <i 
+                        className="huge circular camera retro icon" 
+                        style={{ alignContent: 'right', verticalAlign: 'top' }} 
+                        onClick={this.selectFileToUpload}
+                    >
+                    </i>
+                </span>
+            );
         }
-
-        if (this.state.currentNoOfFiles < this.maxNoOfFiles) {
-            showProfileImg.push(<span key="new"><i className="huge circular camera retro icon" style={{ alignContent: 'right', verticalAlign: 'top' }} onClick={this.selectFileToUpload}></i></span>);
+        else {
+            showProfileImg = (
+                <span>
+                    <img 
+                        style={{ height: 112, width: 112, borderRadius: 55 }} 
+                        className="ui small" 
+                        src={this.state.imageSrc} alt="Image Not Found" 
+                        onClick={this.selectFileToUpload}
+                    />
+                </span>
+            );
         }
 
         return (
