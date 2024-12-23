5a690605c40d4380d7878d5d4e9379a1b6b9a56d
Andres Marin <andres.marin@gmx.com>
2024-12-23 11:19:27 +1100

    Adjusted layout

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/CompanyProfile.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/CompanyProfile.jsx
@@ -7,6 +7,19 @@ export default class CompanyProfile extends React.Component {
     }
 
     render() {        
-        return (<p>Company</p>)
-    }
+        return (
+            <div className="ui card">
+                <div className="content">
+                    <div className="center aligned header">MVP Studio</div>
+                    <div className="center aligned address">
+                        <p>Auckland, New Zealand</p>
+                    </div>
+                </div>
+                <div className="extra content">
+                    <div className="center aligned author">
+                        <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/jenny.jpg" />
+                    </div>
+                </div>
+            </div>
+        )    }
 }
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react';
 import ReactPlayer from 'react-player';
 import PropTypes from 'prop-types'
-import { Popup, Icon } from 'semantic-ui-react'
+import { Card, Grid, Button, Image, Popup, Icon } from 'semantic-ui-react'
 
 export default class TalentCard extends React.Component {
     constructor(props) {
@@ -12,8 +12,50 @@ export default class TalentCard extends React.Component {
     
     
     render() {
-        return(<p>Talent</p>)
-       
-    }
+        return (
+        <Card fluid>
+            <Card.Content>
+            <Grid>
+                <Grid.Row columns={2}>
+                <Grid.Column width={6}>
+                    <Image
+                    src="https://via.placeholder.com/150"
+                    size="tiny"
+                    circular
+                    style={{ marginTop: "5px" }}
+                    />
+                </Grid.Column>
+                <Grid.Column width={10}>
+                    <Card.Header>
+                    Ru (Talent) Ng
+                    <Icon name="star outline" style={{ float: "right" }} />
+                    </Card.Header>
+                    <Card.Meta>Talent snapshot</Card.Meta>
+                    <Card.Description>
+                    <p>
+                        <strong>Current Employer:</strong> ABC
+                    </p>
+                    <p>
+                        <strong>Visa Status:</strong> Citizen
+                    </p>
+                    <p>
+                        <strong>Position:</strong> Software Developer
+                    </p>
+                    </Card.Description>
+                </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            </Card.Content>
+            <Card.Content extra>
+            <Button.Group fluid>
+                <Button icon="video" />
+                <Button icon="file alternate" />
+                <Button icon="linkedin" />
+                <Button icon="github" />
+                <Button icon="c sharp" />
+            </Button.Group>
+            </Card.Content>
+        </Card>
+        );
+    };
 }
-
