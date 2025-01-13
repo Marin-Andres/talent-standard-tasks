0bf20398e1382e07b5b183533dc4fa8cd1066602
Andres Marin <andres.marin@gmx.com>
2025-01-13 16:34:38 +1100

    Switch video/profile functionality

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
@@ -6,50 +6,79 @@ import { Card, Grid, Button, Image, Popup, Icon } from 'semantic-ui-react'
 export default class TalentCard extends React.Component {
     constructor(props) {
         super(props);
-       
+        this.state = {
+            showVideo: true
+        };
+       this.showVideo = this.showVideo.bind(this);
+       this.showTalentSnapshot = this.showTalentSnapshot.bind(this);
     };
 
-    
+    showVideo() {
+        this.setState({ showVideo: true });
+    };
+
+    showTalentSnapshot() {
+        this.setState({ showVideo: false });
+    };
     
     render() {
-        return (
-        <Card fluid>
-            <Card.Content>
-            <Grid>
-                <Grid.Row columns={2}>
-                <Grid.Column width={6}>
+        const { showVideo } = this.state;
+        const switchButton = showVideo ? (
+            <Button icon="user" onClick={this.showTalentSnapshot} />
+        ) : (
+            <Button icon="video" onClick={this.showVideo} />
+        );
+        const cardContentRow = showVideo ? (
+            <Grid.Row columns={1}>
+                <ReactPlayer
+                    url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                    playing={true}
+                    controls={true}
+                    width='100%'
+                    height='100%'
+                />
+            </Grid.Row>
+        ) : (
+            <Grid.Row columns={2}>
+                <Grid.Column width={8}>
                     <Image
-                    src="https://via.placeholder.com/150"
-                    size="tiny"
-                    circular
-                    style={{ marginTop: "5px" }}
+                        src="https://via.placeholder.com/150"
+                        size="tiny"
+                        circular
+                        style={{ marginTop: "5px" }}
                     />
                 </Grid.Column>
-                <Grid.Column width={10}>
-                    <Card.Header>
-                    Ru (Talent) Ng
-                    <Icon name="star outline" style={{ float: "right" }} />
-                    </Card.Header>
+                <Grid.Column width={8}>
                     <Card.Meta>Talent snapshot</Card.Meta>
                     <Card.Description>
-                    <p>
-                        <strong>Current Employer:</strong> ABC
-                    </p>
-                    <p>
-                        <strong>Visa Status:</strong> Citizen
-                    </p>
-                    <p>
-                        <strong>Position:</strong> Software Developer
-                    </p>
+                        <p>
+                            <strong>Current Employer:</strong> ABC
+                        </p>
+                        <p>
+                            <strong>Visa Status:</strong> Citizen
+                        </p>
+                        <p>
+                            <strong>Position:</strong> Software Developer
+                        </p>
                     </Card.Description>
                 </Grid.Column>
-                </Grid.Row>
+            </Grid.Row>
+        );            
+        return (
+        <Card fluid>
+            <Card.Header>
+                Ru (Talent) Ng
+                <Icon name="star outline" style={{ float: "right" }} />
+            </Card.Header>
+            <Card.Content>
+            <Grid>
+                {cardContentRow}
             </Grid>
             </Card.Content>
             <Card.Content extra>
             <Button.Group fluid>
-                <Button icon="video" />
-                <Button icon="file alternate" />
+                {switchButton}
+                <Button icon="file pdf outline" />
                 <Button icon="linkedin" />
                 <Button icon="github" />
             </Button.Group>
